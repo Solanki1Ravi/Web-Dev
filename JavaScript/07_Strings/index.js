4c16b30a3421b4542d1b679e8b4ded542c948fe2
Solanki1Ravi <148341765+Solanki1Ravi@users.noreply.github.com>
2024-07-18 05:19:33 +0000

    Strings

---
+++ JavaScript/07_Strings/index.js
@@ -0,0 +1,32 @@
+/*
+1.We can use single or double quotes for creating a string 
+2. Strings are immutable means after creation we can't change the string.
+
+
+*/
+
+let a = "Ravindra singh";
+let b = 'Ravi Solanki'
+// console.log(a);
+// console.log(b);
+
+// ---------Template Strings----------
+
+/*
+1.Templates are strings enclosed in backticks (`This is a template string`).
+
+2.Templates allow single and double quotes inside a string
+
+*/
+
+// let c = `Ravindra "Solanki"`
+let real_name = `Ravindra Solanki`
+let age = 22
+// console.log(real_name);
+
+
+// console.log(`Hii!! My name is ${real_name} and I'm ${age} years old.`);
+
+
+
+
---
+++ JavaScript/07_Strings/stringMethods.js
@@ -0,0 +1,21 @@
+let real_name = `             Ravindra Singh       lsdflkjdsafsdfljkdslkajfdsfsdf                `;
+let surname = " Solanki"
+
+// console.log(real_name.length);
+// console.log(real_name.slice(1,10));
+//console.log(real_name.slice(1)); // it works same as slice(1,real_name.length-1)
+// console.log(real_name.startsWith("ra"));
+// console.log(real_name.startsWith("Ra"));
+// console.log(real_name.endsWith("ki"));
+// console.log(real_name.toUpperCase());
+// console.log(real_name.toLowerCase());
+//console.log(real_name.split(" ")); // returns array of strings 
+// console.log(real_name.charAt(2));
+// console.log(real_name.replace("Solanki","Singh"));
+// console.log(real_name.concat(surname));
+// console.log(real_name);
+// console.log(real_name.trim());
+// console.log(real_name.trimStart());
+// console.log(real_name.trimEnd());
+
+
