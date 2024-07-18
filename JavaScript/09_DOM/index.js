ad997db3c5fe3b3f8a3a120600bd87b6d5444537
Solanki1Ravi <148341765+Solanki1Ravi@users.noreply.github.com>
2024-07-18 05:20:57 +0000

    dom HTML Manuplation

---
+++ JavaScript/09_DOM/index.js
@@ -0,0 +1,21 @@
+// let firstDiv = document.querySelector(".paraDiv")
+// console.log(firstDiv);
+
+// let firstElement = document.createElement("h2");
+// console.log(firstElement);
+// firstElement.textContent = "Ravindra singh"
+// console.log(firstElement);
+
+// firstDiv.insertAdjacentElement("beforeBegin",firstElement)
+// firstDiv.insertAdjacentElement("afterEnd",firstElement)
+// firstDiv.insertAdjacentElement("afterBegin",firstElement)
+// firstDiv.insertAdjacentElement("beforeEnd",firstElement)
+
+
+
+
+// ----Removal of element 
+let parent = document.querySelector(".paraDiv")
+let child = document.querySelector("#fpara")
+
+parent.removeChild(child)
