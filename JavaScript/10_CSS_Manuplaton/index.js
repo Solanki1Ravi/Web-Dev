12569d4656d4b3b8ef43c1d66c10ffd02870912e
Solanki1Ravi <148341765+Solanki1Ravi@users.noreply.github.com>
2024-07-20 04:42:49 +0000

    css dom manuplation

---
+++ JavaScript/10_CSS_Manuplaton/index.js
@@ -0,0 +1,34 @@
+
+let secondDiv = document.getElementById("sdiv")
+secondDiv
+
+secondDiv.className 
+secondDiv.className = "wanda spidy"
+'wanda spidy'
+    secondDiv
+
+
+
+let thirdDiv = document.querySelector("#sdiv")
+undefined
+thirdDiv
+
+thirdDiv.classList
+
+thirdDiv.toggleAttribute("ironman")
+true
+thirdDiv
+
+
+thirdDiv.classList.add("wanda")
+thirdDiv
+
+thirdDiv.classList.remove("wanda")
+thirdDiv
+
+thirdDiv.classList.toggle("thor")
+false
+thirdDiv
+
+thirdDiv.classList.toggle("thor")
+
